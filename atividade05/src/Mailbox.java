c765181935978376da2d9bd5f830d7f47e5f2fe4
mentejoao <skhulzxd@gmail.com>
2025-04-11 18:48:54 -0300

    atividade 05 - mailbox

---
+++ atividade05/src/Mailbox.java
@@ -0,0 +1,11 @@
+public class Mailbox {
+
+    private static String message;
+    private void storageMessage(String mensagem){
+        if(this.message == null){
+            this.message = mensagem;
+        }
+    }
+
+
+}
