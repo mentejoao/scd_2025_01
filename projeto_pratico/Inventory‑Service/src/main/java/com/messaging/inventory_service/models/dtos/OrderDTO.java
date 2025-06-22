8e3e45c9442f589b6fb319a679657dadfc637c65
jongas124 <kossos124@gmail.com>
2025-06-22 04:27:50 -0300

    fix: change incorrect project name

--- projeto_pratico/Inventory‑Service/src/main/java/com/messaging/inventory_service/models/dtos/OrderDTO.java
+++
@@ -1,7 +0,0 @@
-package com.messaging.inventory_service.models.dtos;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
-
-public record OrderDTO (UUID orderId, LocalDateTime timestamp,  List<String> items) {}
